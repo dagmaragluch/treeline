ccfc39cc505d76cd4b1845ed5905d8a442444948
Chakinn <krol.maciej10@gmail.com>
2020-05-07 21:52:45 +0200

    camera has now position parameter in constructor

--- treeline/__main__.py
+++ treeline/__main__.py
@@ -3,17 +3,18 @@ from treeline.engine.camera import Camera
 from treeline.engine.actor import Actor
 import pygame
 
+
 class TestActor(Actor):
     def on_event(self, event):
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             print("Got it")
 
+
 if __name__ == '__main__':
     engine = Engine()
-    camera = Camera()
-    camera.position = (0, 0)
+    camera = Camera((0, 0))
     engine.set_camera(camera)
-    testActor = TestActor()
+    testActor = TestActor((1, 1))
     engine.add_actor(testActor)
     engine.register_for_event(testActor, pygame.KEYDOWN)
     engine.start()
--- treeline/engine/camera.py
+++ treeline/engine/camera.py
@@ -1,8 +1,10 @@
+from typing import Tuple
+
 from treeline.engine.actor import Actor
 
 
 class Camera(Actor):
 
-    def __init__(self):
-        Actor.__init__(self)
+    def __init__(self, position: Tuple[int, int]):
+        Actor.__init__(self, position)
         pass
