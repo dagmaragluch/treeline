b446aaced3a4eb5181efe4f43e79c30d99eeb419
Chakinn <krol.maciej10@gmail.com>
2020-05-15 16:26:07 +0200

    made internal "board" variable in Board class private/hidden, removed redundant index checks in get_field

--- treeline/model/board.py
+++ treeline/model/board.py
@@ -21,9 +21,9 @@ class Board:
     """
 
     def __init__(self, file: str):
-        self.board = self._create_board(self.get_data(file))
-        self.width = self.board.shape[1]
-        self.height = self.board.shape[0]
+        self._board = self._create_board(self.get_data(file))
+        self.width = self._board.shape[1]
+        self.height = self._board.shape[0]
 
     def _create_board(self, map_in_array: np.ndarray) -> np.ndarray:
         rows = map_in_array.shape[0]
@@ -45,21 +45,21 @@ class Board:
         list_of_neighbours = []
         x = field.position[0]
         y = field.position[1]
-        board_width = self.board.shape[1]
-        board_height = self.board.shape[0]
+        board_width = self._board.shape[1]
+        board_height = self._board.shape[0]
 
         if (y + 2) < board_width:
-            list_of_neighbours.append(self.board[x][y + 2])
+            list_of_neighbours.append(self._board[x][y + 2])
         if (x + 1) < board_width and (y - 1) >= 0:
-            list_of_neighbours.append(self.board[x + 1][y - 1])
+            list_of_neighbours.append(self._board[x + 1][y - 1])
         if (x - 1) >= 0 and (y - 1) >= 0:
-            list_of_neighbours.append(self.board[x - 1][y - 1])
+            list_of_neighbours.append(self._board[x - 1][y - 1])
         if (y - 2) >= 0:
-            list_of_neighbours.append(self.board[x][y - 2])
+            list_of_neighbours.append(self._board[x][y - 2])
         if (x - 1) >= 0 and (y + 1) < board_height:
-            list_of_neighbours.append(self.board[x - 1][y + 1])
+            list_of_neighbours.append(self._board[x - 1][y + 1])
         if (x + 1) < board_width and (y + 1) < board_height:
-            list_of_neighbours.append(self.board[x + 1][y + 1])
+            list_of_neighbours.append(self._board[x + 1][y + 1])
 
         LOGGER.info("Found %d neighbours for (%d, %d) field)", len(list_of_neighbours), x, y)
         return list_of_neighbours
@@ -68,7 +68,7 @@ class Board:
         for x in range(0, self.height):
             for y in range(0, self.width):
                 if (x + y) % 2 == 0:
-                    yield self.board[x][y]
+                    yield self._board[x][y]
 
     @staticmethod
     def get_data(file_name: str) -> np.ndarray:
@@ -76,10 +76,7 @@ class Board:
         return my_data
 
     def get_field(self, x: int, y: int) -> Field:
-        if 0 < x < self.height and 0 < y < self.width:
-            return self.board[x][y]
-        else:
-            raise IndexError
+        return self._board[x][y]
 
     def get_random_field(self) -> Field:
         x = 0
