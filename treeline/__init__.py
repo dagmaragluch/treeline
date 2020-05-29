68076a09079d9fc28d39c9df07ac5fd193ff00a0
Chakinn <krol.maciej10@gmail.com>
2020-05-29 17:14:58 +0200

    logging format

--- treeline/__init__.py
+++ treeline/__init__.py
@@ -2,4 +2,8 @@
 main module of the game
 """
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(name)s [%(levelname)s]: %(message)s",
+    datefmt="%H:%M:%S"
+)
