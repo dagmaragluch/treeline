67e706a6a32a48be844dcce57511f37552b18b15
Tomasz <cansisti@outlook.com>
2020-06-03 18:47:35 +0200

    grass and mountain as sprites

--- treeline/model/field_config.py
+++ treeline/model/field_config.py
@@ -5,11 +5,11 @@ import pygame
 forest = pygame.image.load('./resources/graphics/terrain/forest.png')
 
 hexagons = {
-    "grass": Hexagon(color=(82, 235, 52)),
+    "grass": Sprite(pygame.image.load('./resources/graphics/terrain/grass.png')),
     "grass_highlight": Hexagon(color=(102, 255, 72)),
     "grass_red": Hexagon(color=(255, 235, 52)),  # not red at all
-    "forest": Sprite(forest),
+    "forest": Sprite(pygame.image.load('./resources/graphics/terrain/forest.png')),
     "forest_highlight": Hexagon(color=(41, 137, 22)),
-    "mountain": Hexagon(color=(97, 77, 50)),
+    "mountain": Sprite(pygame.image.load('./resources/graphics/terrain/mountain.png')),
     "mountain_highlight": Hexagon(color=(117, 97, 70))
 }
