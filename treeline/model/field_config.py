d7b4a6b94c83e70aeb8bc20b3ee30adc3cd6f481
Tomasz <cansisti@outlook.com>
2020-06-05 00:21:03 +0200

    other field highlights

--- treeline/model/field_config.py
+++ treeline/model/field_config.py
@@ -3,10 +3,10 @@ import pygame
 
 hexagons = {
     "grass": Sprite(pygame.image.load('./resources/graphics/terrain/grass.png')),
-    "grass_highlight": Sprite(pygame.image.load('./resources/graphics/terrain/grass.png')), # TODO
-    "grass_red": Sprite(pygame.image.load('./resources/graphics/terrain/grass.png')), # TODO
+    "grass_highlight": Sprite(pygame.image.load('./resources/graphics/terrain/grass_highlight.png')),
+    "grass_red": Sprite(pygame.image.load('./resources/graphics/terrain/forest_red.png')), # XD
     "forest": Sprite(pygame.image.load('./resources/graphics/terrain/forest.png')),
     "forest_highlight": Sprite(pygame.image.load('./resources/graphics/terrain/forest_highlight.png')),
     "mountain": Sprite(pygame.image.load('./resources/graphics/terrain/mountain.png')),
-    "mountain_highlight": Sprite(pygame.image.load('./resources/graphics/terrain/mountain.png')) # TODO
+    "mountain_highlight": Sprite(pygame.image.load('./resources/graphics/terrain/mountain_highlight.png'))
 }
