2a87c5f33010f0dc0f648ee04e3915235d782e6f
sliderro <arek.debski2@gmail.com>
2020-06-07 20:30:43 +0200

    Sender updated with start command

--- treeline/network/sender.py
+++ treeline/network/sender.py
@@ -14,13 +14,13 @@ class Sender:
     def send_take(self, field: Field):
         x, y = field.position
         msg = "TAKE {} {}".format(x, y)
-        LOGGER.debug(msg)
+        LOGGER.debug("Sent %s", msg)
         self.sender.sendall(bytes(msg, 'UTF-8'))
 
     def send_build(self, building_type: str, field: Field):
         x, y = field.position
         msg = "BUILD {} {} {}".format(x, y, building_type)
-        LOGGER.debug(msg)
+        LOGGER.debug("Sent %s", msg)
         self.sender.sendall(bytes(msg, 'UTF-8'))
 
     def send_add_worker(self, field: Field):
@@ -32,22 +32,28 @@ class Sender:
     def send_remove_worker(self, field: Field):
         x, y = field.position
         msg = "REMOVE {} {}".format(x, y)
-        LOGGER.debug(msg)
+        LOGGER.debug("Sent %s", msg)
+        self.sender.sendall(bytes(msg, 'UTF-8'))
+
+    def send_start(self, field: Field):
+        x, y = field.position
+        msg = "START {} {}".format(x, y)
+        LOGGER.debug("Sent %s", msg)
         self.sender.sendall(bytes(msg, 'UTF-8'))
 
     def send_end_turn(self):
         msg = "END"
-        LOGGER.debug(msg)
+        LOGGER.debug("Sent %s", msg)
         self.sender.sendall(bytes(msg, 'UTF-8'))
 
     def send_game_over(self):
         msg = "OVER"
-        LOGGER.debug(msg)
+        LOGGER.debug("Sent %s", msg)
         self.sender.sendall(bytes(msg, 'UTF-8'))
 
     def send_ready(self):
         msg = "READY"
-        LOGGER.debug(msg)
+        LOGGER.debug("Sent %s", msg)
         self.sender.sendall(bytes(msg, 'UTF-8'))
 
     def close(self):
